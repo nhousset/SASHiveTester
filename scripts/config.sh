8ccc255ac51f9339f495d3df1504ded35f2cd8ec
Nicolas Housset <nicolas.housset@gmail.com>
2022-09-16 14:08:08 +0200

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+RED='\033[031m'
+GREEN='\033[032m'
+YELLOW='\033[033m'
+BLUE='\033[034m'
+NC='\033[0m' 
 
 if [ -z ${JAVA_HOME} ]
 then
@@ -15,12 +20,13 @@ if [ -z ${HIVE_HOME} ]
 then
     export HIVE_HOME=../hive/1.2.1
 fi 
-
-
 export PATH=$PATH:$HADOOP_HOME
 
-echo ${JAVA_HOME}
-echo ${HADOOP_HOME}
-echo ${HIVE_HOME}
+echo -en "JAVA_HOME :${GREEN}$JAVA_HOME${NC}\n"
+echo -en "HADOOP_HOME :${GREEN}$HADOOP_HOME${NC}\n"
+echo -en "HIVE_HOME :${GREEN}$HIVE_HOME${NC}\n"
+echo -en "\n\n"
+echo -en "VIYA_VERSION :${GREEN} $HIVE_HOME/bin/beeline --incremental=true ${NC}\n"
+echo -en "\n\n"
 
 $HIVE_HOME/bin/beeline --incremental=true
