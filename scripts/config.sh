2f2a169fcbe995c643dd98d6307ed475b859f121
Nicolas Housset <nicolas.housset@gmail.com>
2022-09-16 14:04:41 +0200

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,23 +3,20 @@
 
 if [ -z ${JAVA_HOME} ]
 then
-    JAVA_HOME=/usr/lib/jvm/jre-1.8.0 
+    export JAVA_HOME=/usr/lib/jvm/jre-1.8.0 
 fi 
 
 if [ -z ${HADOOP_HOME} ]
 then
-    HADOOP_HOME=../hadoop-dist/hadoop-2.7.3
+    export HADOOP_HOME=../hadoop-dist/hadoop-2.7.3
 fi 
 
 if [ -z ${HIVE_HOME} ]
 then
-    HIVE_HOME=../hive/1.2.1
+    export HIVE_HOME=../hive/1.2.1
 fi 
 
 
-export ${JAVA_HOME}
-export HADOOP_HOME=../hadoop-dist/hadoop-2.7.3
-export HIVE_HOME=../hive/1.2.1
 export PATH=$PATH:$HADOOP_HOME
 
 echo ${JAVA_HOME}
