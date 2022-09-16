e91e32321e9111d00680748f2e44d4331d099c24
Nicolas Housset <nicolas.housset@gmail.com>
2022-09-16 11:48:01 +0200

    Update boucle.sh

--- boucle.sh
+++ boucle.sh
@@ -1,5 +1,5 @@
-logfile=/tmp/$$.log
-exec > $logfile 2>&1
+#logfile=/tmp/$$.log
+#exec > $logfile 2>&1
 
 URL=$1
 USER=$2
@@ -13,7 +13,7 @@ fi
 for i in 1 2 3 4 5 6 7 8 9 10
 do
 echo "RUN ========================= "$i
-time $HIVE_HOME/bin/beeline -u $URL --incremental=true -n $USER -p $PASS 2>/tmp/run_err_${i}.log >/tmp/run_${i}.log &<<EOF
+time $HIVE_HOME/bin/beeline -u $URL --incremental=true -n $USER -p $PASS<<EOF
 show databases;
 use in_usage_nacsel_expo_dt;
 show tables;
