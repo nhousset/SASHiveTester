62c6fe099b77b84b738bed3faae33e85312af1f3
Nicolas Housset <nicolas.housset@gmail.com>
2023-02-28 18:16:55 +0100

    Update boucle.sh

--- scripts/boucle.sh
+++ scripts/boucle.sh
@@ -12,7 +12,7 @@ then
  echo $0 "<URL JDBC> <USER> <PASSWORD>"
   exit 1;
 fi
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 do
 echo "RUN ========================= "$i
 time $HIVE_HOME/bin/beeline -u $URL --incremental=true -n $USER -p $PASS &<<EOF
